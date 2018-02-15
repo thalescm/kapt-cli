a74e80fd2c3b51601cc6db462f993b80d1dba0b5
Thales Machado <thales.mchd@gmail.com>
2018-02-15 15:58:59 -0200

    kotlin only ap working

--- sample/br/com/thales/buckt/AnnotatedMain.kt
+++ sample/br/com/thales/buckt/AnnotatedMain.kt
@@ -3,12 +3,17 @@ package br.com.thales.buckt
 @ViewBinder
 class NameViewBinder
 
+// @ViewModel
+// class NameViewModel
+
 fun main(args: Array<String>) {
     val binder = NameViewBinder_()
     binder.bindViews()
 
     val otherBinder = AnnotatedMainJava()
     otherBinder.bindViewsToo()
+
+    // val model = NameViewModel_()
 }
 
 fun out(num1: Int): Float {
